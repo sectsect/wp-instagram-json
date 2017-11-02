bb2ec7da779112b97cf8f81fb8de015990843b15
SECT <info@sectwebstudio.com>
2017-11-03 00:20:01 +0900

    Code cleanup for PHPCS

--- functions/functions.php
+++ functions/functions.php
@@ -27,12 +27,12 @@
  */
 function wp_instagram_json_is_s3() {
 	$s3_enable = get_option( 'wp_instagram_json_s3_enable' );
-	$s3_key = get_option( 'wp_instagram_json_aws_credentials_key' );
+	$s3_key    = get_option( 'wp_instagram_json_aws_credentials_key' );
 	$s3_secret = get_option( 'wp_instagram_json_aws_credentials_secret' );
 	$s3_region = get_option( 'wp_instagram_json_s3_region' );
 	$s3_bucket = get_option( 'wp_instagram_json_s3_bucket' );
-	$s3_path = get_option( 'wp_instagram_json_s3_path' );
-	$bool = ( $s3_enable && $s3_key && $s3_secret && $s3_region && $s3_bucket && $s3_path ) ? true : false;
+	$s3_path   = get_option( 'wp_instagram_json_s3_path' );
+	$bool      = ( $s3_enable && $s3_key && $s3_secret && $s3_region && $s3_bucket && $s3_path ) ? true : false;
 
 	return $bool;
 }
@@ -46,20 +46,20 @@ function wp_instagram_json_var() {
 	if ( wp_instagram_json_is_s3() ) {
 		if ( get_option( 'wp_instagram_json_s3_custom_url' ) ) {
 			$customurl = rtrim( esc_url( get_option( 'wp_instagram_json_s3_custom_url' ) ), '/' );
-			$url = $customurl . '/' . get_option( 'wp_instagram_json_s3_path' ) . '/instagram.json';
+			$url       = $customurl . '/' . get_option( 'wp_instagram_json_s3_path' ) . '/instagram.json';
 		} else {
 			$url = 'https://' . get_option( 'wp_instagram_json_s3_bucket' ) . '.s3.amazonaws.com/' . get_option( 'wp_instagram_json_s3_path' ) . '/instagram.json';
 		}
 	} else {
 		$url = plugin_dir_url( dirname( __FILE__ ) ) . 'json/instagram.json';
 	}
-	$cache = get_option( '_transient_wp_instagram_json' );
+	$cache    = get_option( '_transient_wp_instagram_json' );
 	$datetime = ( $cache ) ? $cache : false;
-	$array = array(
+	$array    = array(
 		'json_url'      => $url,
 		'generate_time' => $datetime,
 	);
-	$obj = json_encode( $array );
+	$obj      = json_encode( $array );
 	echo '<script>var wp_ig_json = ' . $obj . ';</script>';
 }
 add_action( 'wp_head', 'wp_instagram_json_var' );
--- includes/class-wp-instagram-json.php
+++ includes/class-wp-instagram-json.php
@@ -41,28 +41,28 @@ class WP_Instagram_JSON {
 	 * @return void "description".
 	 */
 	public function generate_json_file() {
-		$tname = 'wp_instagram_json';
+		$tname     = 'wp_instagram_json';
 		$transient = get_transient( $tname );
 		if ( empty( $transient ) ) {
 			$user_account = get_option( 'wp_instagram_json_account_name' );
 			$access_token = get_option( 'wp_instagram_json_access_token' );
-			$count = ( get_option( 'wp_instagram_json_count' ) ) ? (int) get_option( 'wp_instagram_json_count' ) : 20;
-			$date = date_i18n( 'Y/m/d H:i:s' );
+			$count        = ( get_option( 'wp_instagram_json_count' ) ) ? (int) get_option( 'wp_instagram_json_count' ) : 20;
+			$date         = date_i18n( 'Y/m/d H:i:s' );
 			if ( $user_account && $access_token ) {
 				$user_api_url = 'https://api.instagram.com/v1/users/search?q=' . $user_account . '&access_token=' . $access_token;
-				$user_data = json_decode( file_get_contents( $user_api_url ) );
+				$user_data    = json_decode( file_get_contents( $user_api_url ) );
 				foreach ( $user_data->data as $user_data ) {
 					if ( $user_account == $user_data->username ) {
 						$user_id = $user_data->id;
 					}
 				}
-				$api_url = 'https://api.instagram.com/v1/users/' . $user_id . '/media/recent?access_token=' . $access_token . '&count=' . $count;
-				$ig_data = json_decode( file_get_contents( $api_url ), true );
+				$api_url   = 'https://api.instagram.com/v1/users/' . $user_id . '/media/recent?access_token=' . $access_token . '&count=' . $count;
+				$ig_data   = json_decode( file_get_contents( $api_url ), true );
 				$date_data = array(
 					'file_generate_datetime' => $date,
 				);
-				$res = array_merge_recursive( $ig_data, $date_data );
-				$obj = json_encode( $res );
+				$res       = array_merge_recursive( $ig_data, $date_data );
+				$obj       = json_encode( $res );
 
 				$filepath = plugin_dir_path( dirname( __FILE__ ) ) . 'json/instagram.json';
 				file_put_contents( $filepath, $obj );
@@ -80,27 +80,27 @@ class WP_Instagram_JSON {
 	 * @return void "description".
 	 */
 	public function put_object_to_s3() {
-		$sdkconfig = [
+		$sdkconfig  = [
 			'region'      => get_option( 'wp_instagram_json_s3_region' ),
 			'version'     => 'latest',
 			'credentials' => [
-				'key'     => get_option( 'wp_instagram_json_aws_credentials_key' ),
-				'secret'  => get_option( 'wp_instagram_json_aws_credentials_secret' ),
+				'key'    => get_option( 'wp_instagram_json_aws_credentials_key' ),
+				'secret' => get_option( 'wp_instagram_json_aws_credentials_secret' ),
 			],
 		];
-		$sdk = new Aws\Sdk( $sdkconfig );
-		$s3 = $sdk->createS3();
+		$sdk        = new Aws\Sdk( $sdkconfig );
+		$s3         = $sdk->createS3();
 		$bucketname = get_option( 'wp_instagram_json_s3_bucket' );
-		$keyname = get_option( 'wp_instagram_json_s3_path' ) . '/instagram.json';
-		$srcfile = plugin_dir_path( dirname( __FILE__ ) ) . 'json/instagram.json';
-		$result = $s3->putObject([
-			'Bucket' => $bucketname,
-			'Key' => $keyname,
-			'SourceFile' => $srcfile,
+		$keyname    = get_option( 'wp_instagram_json_s3_path' ) . '/instagram.json';
+		$srcfile    = plugin_dir_path( dirname( __FILE__ ) ) . 'json/instagram.json';
+		$result     = $s3->putObject([
+			'Bucket'      => $bucketname,
+			'Key'         => $keyname,
+			'SourceFile'  => $srcfile,
 			'ContentType' => 'application/json',
 		]);
-		$result = $result->toArray();
-		$object = json_decode( json_encode( $result ) );
+		$result     = $result->toArray();
+		$object     = json_decode( json_encode( $result ) );
 		$statuscode = $object->{'@metadata'}->statusCode;
 		$fileexists = $s3->doesObjectExist( $bucketname, $keyname );
 		if ( 200 === $statuscode && $fileexists ) {
