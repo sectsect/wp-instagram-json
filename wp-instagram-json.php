721c6a77682d2a990914e8906776bb175b8a23c1
SECT <info@sectwebstudio.com>
2018-01-23 14:40:00 +0900

    :tada: Update version v1.1.5

--- wp-instagram-json.php
+++ wp-instagram-json.php
@@ -7,7 +7,7 @@
  * Author URI:      https://www.ilovesect.com/
  * Text Domain:     wp-instagram-json
  * Domain Path:     /languages
- * Version:         1.1.4
+ * Version:         1.1.5
  *
  * @package         WP_Instagram_JSON
  */
